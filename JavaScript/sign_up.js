2fc800567690da43f4912831d9980615e4a79d90
MyungHa Hwang <audgk425@gmail.com>
2021-11-19 17:25:22 +0900

    Fix Ver.5

--- JavaScript/sign_up.js
+++ JavaScript/sign_up.js
@@ -3,7 +3,7 @@ function idCheck() {
     const id = document.getElementById('id').value;
     console.log(id);
 
-    const exp = /^(?=.*[a-z])(?=.*\d)[a-z\d]{6,10}$/
+    const exp = /^(?=.*[a-z])(?=.*\d)[a-z\d]{8,20}$/
 
     // span 이니까 innerHTML
     const idCheck = document.getElementById('idCheck');
@@ -25,19 +25,35 @@ function pwCheck() {
     const pw = document.getElementById('pw').value;
     console.log(pw);
 
-    const exp = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[-_!@#])(?=.*\d)[a-z\d]{6,10}$/
+    var exp = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[-_!@#])(?=.*\d)[a-zA-Z\d-_!@#]{5,30}$/
 
     const pwCheck = document.getElementById('pwCheck');
     
     const pwLength = pw.length;
 
-    if (pwLength.match(exp)) {
-        pwCheck.innerHTML = '적합한 비밀번호입니다.';
-        pwCheck.style.color ='green';
+    if (pwLength == 0) {
+        pwCheck.innerHTML = '필수요소입니다.';
+        pwCheck.style.color = 'red';
     } else {
-        pwCheck.innerHTML = '적합하지 않은 비밀번호입니다.';
-        pwCheck.style.color = 'red'
+        if (pw.match(exp)) {
+            pwCheck.innerHTML = '적절한 비밀번호입니다.';
+            pwCheck.style.color = 'green';
+        } else {
+            pwCheck.innerHTML = '부적절한 비밀번호입니다.';
+            pwCheck.style.color = 'red';
+        }
     }
+
+    // if (pwLength == 0) {
+    //     pwCheck.innerHTML = '필수요소입니다.';
+    //     pwCheck.style.color = 'red';
+    // } else if (pw.match(exp)) {
+    //     pwCheck.innerHTML = '적합한 비밀번호입니다.';
+    //     pwCheck.style.color ='green';
+    // } else {
+    //     pwCheck.innerHTML = '적합하지 않은 비밀번호입니다.';
+    //     pwCheck.style.color = 'red'
+    // }
 
 }
 
@@ -56,10 +72,21 @@ function pwCheckResult() {
     }
 }
 
-function email() {
+function emailSelect() {
     const emailResult = document.getElementById('emailResult');
     const email = document.getElementById('email').value;
     emailResult.value = email;
+}
+
+function email() {
+    const email = document.getElementById('em');
+    const emailLength = em.length;
+    const emailCheck = document.getElementById('emailCheck');
+
+    if (emailLength == 0) {
+        emailCheck.innerHTML = '필수항목입니다.'
+        emailCheck.style.color = 'red';
+    }
 }
 
 function record() {
